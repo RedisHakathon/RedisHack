9b4614a28d014740d4110c43a5614c5c3cc7c073
Omar Abdullahi Ahmed <85021780+Abdullahi-Ahmed@users.noreply.github.com>
2022-11-07 19:23:37 +0300

    delete unnecessary files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-import streamlit as st
-
-INDEX_NAME = "index"
-REDIS_HOST = st.write("REDIS_HOST:", st.secrets["REDIS_HOST"])
-REDIS_PORT = st.write("REDIS_PORT:", st.secrets["REDIS_PORT"])
-REDIS_DB = st.write("REDIS_DB:", st.secrets["REDIS_DB"])
-REDIS_PASSWORD = st.write("REDIS_PASSWORD:", st.secrets["REDIS_PASSWORD"])
-REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"

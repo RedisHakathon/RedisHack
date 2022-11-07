bd2d012a7f61a54b41dfc9873b3b85376ea05bd3
Omar Abdullahi Ahmed <aaobare2@gmail.com>
2022-11-07 16:45:43 +0300

    styling

--- app.py
+++ app.py
@@ -65,9 +65,19 @@ if selected == "Paper Recommendation":
             results =  redis_conn.ft(INDEX_NAME).search(query, query_params = query_param)
             
             for p in results.docs:
+                st.markdown(
+                    f""" <div class="card border-success mb-3" style="max-width:30rem;position: relative; background-color:#ccc; border-radius:10px;">   
+                    <div class="card-header bg-transparent border-success" style="color:#000000; text-align:left;color: #2146C7;margin:10px;"><u>{p.titles}</u></div> 
+                    <div class="card-body text-success">
+                      <p class="card-title" style="color:#000000; margin:20px;;margin-top:-5px"><i>{p.author}:({p.year})</i></p>
+                    </div>
+                    </div> """,
+                    unsafe_allow_html=True,
+                    )
+
                 st.markdown(    
                     f""" 
-                   <p class="card-title" style="color:#000000; margin:20px;;margin-top:-5px"><i>{p.auth}:({p.year})</i></p> 
+                   <p style="color:#000000; margin:20px;;margin-top:-5px"><i>{p.auth}:({p.year})</i></p> 
                     
                     """,
 
